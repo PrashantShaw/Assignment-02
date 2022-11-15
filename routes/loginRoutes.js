f6d10881a857268fd8d09eb826dc6f54fa6f5ca9
PrashantShaw <prashant.shaw.3154@gmail.com>
2022-11-15 18:29:33 +0530

    Corrected status codes

--- routes/loginRoutes.js
+++ routes/loginRoutes.js
@@ -24,7 +24,7 @@ router.post('/register',
             // HASHING THE PASSWORD
             bcrypt.hash(password, 10, async (err, hashedPassword) => {
                 if (err) {
-                    return res.status(400).json({ error: err.message })
+                    return res.status(500).json({ error: err.message })
                 }
                 // CREATING USER
                 const user = await usersModel.create({
@@ -40,7 +40,7 @@ router.post('/register',
             })
 
         }
-        catch (err) { res.status(400).json({ message: err.message }) }
+        catch (err) { res.status(500).json({ message: err.message }) }
     })
 
 
@@ -56,24 +56,28 @@ router.post('/login', async (req, res) => {
         // COMPARING WITH HASHED PASSWORD
         bcrypt.compare(password, user.password, async (err, result) => {
             if (err) {
-                return res.status(404).json({ message: err.message })
+                return res.status(500).json({ message: err.message })
             }
-            if (result) {
-                // GENERATING TOKEN
-                const token = jwt.sign(
-                    { data: user._id },
-                    Auth_config.secret,
-                    { expiresIn: '1h' }
-                )
-                // SENDING TOKEN AS RESPONSE TO USER
-                res.status(200).json({
-                    status: "Success",
-                    token
+            if (!result) {
+                return res.status(401).json({
+                    status: "failed",
+                    message: "Invalid credentials"
                 })
             }
+            // GENERATING TOKEN
+            const token = jwt.sign(
+                { data: user._id },
+                Auth_config.secret,
+                { expiresIn: '1h' }
+            )
+            // SENDING TOKEN AS RESPONSE TO USER
+            res.status(200).json({
+                status: "Success",
+                token
+            })
         })
     }
-    catch (err) { res.status(400).json({ message: err.message }) }
+    catch (err) { res.status(500).json({ message: err.message }) }
 })
 
 
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -24,7 +24,7 @@ router.post("/",
                 data: createdPost
             })
         }
-        catch (err) { res.status(404).json({ message: err.message }) }
+        catch (err) { res.status(500).json({ message: err.message }) }
     })
 // ------------------------------------------------------------------
 
@@ -51,7 +51,7 @@ router.put("/:postId",
                 status: "Success",
             })
         }
-        catch (err) { res.status(404).json({ message: err.message }) }
+        catch (err) { res.status(500).json({ message: err.message }) }
     })
 // ------------------------------------------------------------------
 
@@ -77,7 +77,7 @@ router.delete("/:postId",
                 status: "Successfully deleted",
             })
         }
-        catch (err) { res.status(404).json({ message: err.message }) }
+        catch (err) { res.status(500).json({ message: err.message }) }
     })
 
 
